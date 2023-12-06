9e33109ed051310d96b572f0441cac47a80fb6cd
Allen Paul Cabic <2227312@slu.edu.ph>
2023-12-06 14:35:41 +0800

    Registering account now properly stored in Firestore
    added checking if the login user is admin or user if admin it will redirect to adminPage activity else it will go to main activity

---
+++ app/src/main/java/com/example/kaizentranspo/AdminPage.java
@@ -0,0 +1,16 @@
+package com.example.kaizentranspo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+
+        // Dito ilalagay anh activity ng admin once nag login sya
+    }
+}
