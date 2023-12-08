c602dc6e262af05740763215af0c7fb31a4a6af2
BENNY GIL LACTAOTAO <2233116@slu.edu.ph>
2023-12-08 10:45:35 +0000

    Revert "Merge branch 'Perry's'"
    This reverts merge request !8

--- app/src/main/java/com/example/kaizentranspo/MainActivity.java
+++ app/src/main/java/com/example/kaizentranspo/MainActivity.java
@@ -4,44 +4,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        auth=FirebaseAuth.getInstance();
-        button=findViewById(R.id.logout);
-        textView=findViewById(R.id.userDetails);
-        user = auth.getCurrentUser();
-        if(user==null){
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
-        }
-        else {
-            textView.setText(user.getEmail());
-        }
-        button.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Replace "yourUsername" and "yourPassword" with the actual username and password
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+                Intent intent = new Intent(MainActivity.this, BookActivity.class);
+                if (username.equals("admin") && password.equals("admin")) {
+                    // Successful login
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+                } else {
+                    // Invalid credentials
+                    Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
-
-
     }
 }
--- app/src/main/java/com/example/kaizentranspo/RecyclerViewInterface.java
+++
@@ -1,5 +0,0 @@
-package com.example.kaizentranspo;
-
-public interface RecyclerViewInterface {
-    void onClick(int position);
-}
