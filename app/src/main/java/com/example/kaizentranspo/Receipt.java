c4dd0c12b38459a3b54a4212997f6309ab15102a
KEANU SONN FORTALEZA <2233676@slu.edu.ph>
2023-12-09 06:48:25 +0000

    Revert "Merge branch 'Perry's' into 'login_Register_Done'"
    This reverts merge request !11

--- app/src/main/java/com/example/kaizentranspo/Receipt.java
+++ app/src/main/java/com/example/kaizentranspo/Receipt.java
@@ -18,7 +18,7 @@ public class Receipt extends AppCompatActivity {
         setContentView(R.layout.activity_receipt);
 
         Intent intent = getIntent();
-        String selectedSeat = intent.getStringExtra("selectedSeat");
+        String receivedData = intent.getStringExtra("selectedSeat");
 
         Button confirmButton = findViewById(R.id.confirmButton);
         Button cancelButton = findViewById(R.id.cancelButton);
@@ -46,7 +46,7 @@ public class Receipt extends AppCompatActivity {
         TextView busNum = findViewById(R.id.busNumber_receipt);
         busNum.setText(busNumber);
 
-        seat.setText("Seat #" +selectedSeat);
+        seat.setText(receivedData);
 
 
         thanks.setVisibility(View.INVISIBLE);
@@ -60,27 +60,6 @@ public class Receipt extends AppCompatActivity {
                     @Override
                     public void run() {
                         Intent intent = new Intent(getApplicationContext(), Ticket.class);
-
-                        /**TO BE REMOVED*/
-
-                        intent.putExtra("Selected Seat", "Seat #"+selectedSeat);
-                        intent.putExtra("Destination", destinationText);
-                        intent.putExtra("Departure Time", departure);
-                        intent.putExtra("Bus Number", busNumber);
-
-                        /**TO BE REMOVED*/
-
-                        /**
-                         * Try to pass these variables to database then retrieve in the Ticket class
-                         *
-                         * selectedSeat
-                         * destinationText
-                         * departure
-                         * busNumber
-                         *
-                         * */
-
-
                         startActivity(intent);
                     }
                 }, 2000);
