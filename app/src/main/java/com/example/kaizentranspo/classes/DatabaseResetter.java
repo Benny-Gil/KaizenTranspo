dc2a6c0c37c45e403980b17fc769ffbc959c081d
Allen Paul Cabic <2227312@slu.edu.ph>
2023-12-11 16:26:20 +0800

    Classes for Resseting the contents of the db

--- app/src/main/java/com/example/kaizentranspo/classes/DatabaseResetter.java
+++ app/src/main/java/com/example/kaizentranspo/classes/DatabaseResetter.java
@@ -1,15 +1,12 @@
 package com.example.kaizentranspo.classes;
 
-import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+
 import com.google.firebase.firestore.WriteBatch;
 
 import java.util.HashMap;
