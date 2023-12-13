2ebb4f367c615ecbbbf8b953da1c49dcd7695ad1
keanu <2233676@slu.edu.ph>
2023-12-13 10:33:04 +0800

    created a package for admin side. Added AdminBookings, RemoveBus, BusAdd, and their layouts

---
+++ app/src/main/java/com/example/kaizentranspo/admin/AdminBookings.java
@@ -0,0 +1,16 @@
+package com.example.kaizentranspo.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kaizentranspo.R;
+
+public class AdminBookings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_bookings);
+    }
+}
