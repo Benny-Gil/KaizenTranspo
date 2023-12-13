a5c724f300cf711333274d12e881734355bafb00
Allen Paul Cabic <2227312@slu.edu.ph>
2023-12-13 08:12:14 +0800

    template for admin

---
+++ app/src/main/java/com/example/kaizentranspo/SeatAdminView.java
@@ -0,0 +1,17 @@
+package com.example.kaizentranspo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.kaizentranspo.R;
+
+public class SeatAdminView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_admin_view);
+    }
+}
+/**
+ * ETO UNG SECOND PIC
+ */
